170cdd59551133523d76496bd1a501c50cf0da86
Victor Jauregui <vicjaudom@hotmail.com>
2023-06-27 18:39:02 +0200

    details

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const app = require("./server");
 const Config = require("./src/config/config");
 const { dbConnection } = require("./src/database/database");
@@ -5,7 +7,7 @@ const { dbConnection } = require("./src/database/database");
 dbConnection().then(async function onServerInit() {
   console.log("DB connected");
 
-  app.listen(Config.app.PORT, "0.0.0.0", () => {
+  app.listen(process.env.PORT, "0.0.0.0", () => {
     console.log(`Serving on port ${Config.app.PORT}`);
   });
 });
